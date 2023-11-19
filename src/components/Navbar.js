e25b061edd740493fdeede23aa864a7d9d08b027
Mahmod Hassan <mahmodhasan7788@gmail.com>
2023-11-19 14:21:22 +0600

    navbar responsive done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react';
-import { FaCircleUser } from "react-icons/fa6";
+import React, { useContext, useState } from 'react';
+import { FaBars, FaCircleUser, FaXmark } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
 import icon from '../assets/img/travel-icon.png';
 import { UserContext } from '../context/UserProvider';
@@ -17,15 +17,15 @@ const navLinks = [
 const Navbar = () => {
 
   const {loggedIn, setLoggedIn} = useContext(UserContext);
-  console.log(loggedIn)
-
+  const [show, setShow] = useState(false);
+  console.log(show);
   const handleLogout = () => {
     setLoggedIn(false);
   }
 
 // return jsx
     return (
-        <header  className={`px-4 md:px-10 z-100 bg-white sticky top-0 transition-transform duration-300`}>
+        <header  className={`px-4 md:px-10 z-100 bg-white sticky top-0 transition-transform duration-300 relative`}>
         {/* <div className=" mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8"> */}
           <div className="flex h-16 items-center justify-between">
             <div className="md:flex md:items-center md:gap-12">
@@ -33,9 +33,9 @@ const Navbar = () => {
                   <img className='w-20 h-16' src={icon} alt="" />
               </Link>
             </div>
-            <div className="hidden md:block">
-              <nav aria-label="Global">
-                <ul className="flex items-center gap-6 text-sm">
+            <div>
+              <nav className={`flex flex-col md:flex-row gap-6 ${show ? 'absolute md:static bg-white right-0 top-16 p-10 md:p-0' : 'hidden md:flex'}`}>
+                <ul className={`flex items-center gap-6 text-sm`}>
                  {
                   navLinks.map(({title, path}) => (
                     <li key={path} className='text-lg'>
@@ -44,55 +44,40 @@ const Navbar = () => {
                   ))
                  }
                 </ul>
+                <div>
+                    {
+                      loggedIn ? 
+                      (
+                        <div className='flex gap-4'>
+                          <FaCircleUser className='w-10 h-10' />
+                          <button onClick={handleLogout} className='rounded px-5 py-1 bg-red-600 text-white'>Logout</button>
+                        </div> 
+                      )
+                    :
+                    (
+                      <Link
+                      className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow"
+                      to='/login'
+                    >
+                      Login
+                    </Link>
+                    )
+                    }
+                    
+                </div>
               </nav>
+              <button
+                className={`block md:hidden text-xl`}
+                onClick={() => setShow(!show)}
+              >
+                {show ? (
+                  <FaXmark />
+                ) : (
+                  <FaBars />
+                )}
+              </button>
             </div>
-      
-            <div className="flex items-center gap-4">
-              <div className="hidden md:block">
-                {
-                  loggedIn ? 
-                  (
-                    <div className='flex gap-4'>
-                       <FaCircleUser className='w-10 h-10' />
-                       <button onClick={handleLogout} className='rounded px-5 py-1 bg-red-600 text-white'>Logout</button>
-                    </div> 
-                  )
-                :
-                (
-                  <Link
-                  className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow"
-                  to='/login'
-                >
-                  Login
-                </Link>
-                )
-                }
-              </div>
-      
-               {/* 3 line icon */}
-              <div className="block md:hidden">
-                <button
-                  className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  </svg>
-                </button>
-              </div>
             </div>
-          </div>
-        {/* </div> */}
       </header>
  
     );
